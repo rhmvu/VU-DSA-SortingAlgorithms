dc5b57ad1e50061e549a1167ad8bd823d1da9770
Ruben van der Ham <ravdham@protonmail.com>
2017-09-25 23:44:15 +0200

    layout update

--- src/main/java/Heap.java
+++ src/main/java/Heap.java
@@ -5,74 +5,70 @@ class Heap {
     int heapSize;
     int length;
 
-    Heap(int[] input){
+    Heap (int[] input) {
         array = new int[input.length+1];
-        for(int i = 1; i<=input.length;i++){
+        for (int i = 1; i<=input.length;i++) {
             array[i] = input[i-1];
         }
         length = input.length;
         heapSize = length;
     }
 
-
-    int[] toArray(){
+    int[] toArray() {
         int[] result = new int[length];
-        for (int i = 0; i<length;i++){
+        for (int i = 0; i<length;i++) {
             result[i] = array[i+1];
         }
         return result;
     }
 
-    void sort(){
+    void sort() {
         buildMaxHeap();
-        for(int i = length; i>=2; i--){
+        for (int i = length; i>=2; i--) {
             exchange(ROOT,i);
             heapSize -= 1;
             maxHeapify(ROOT);
         }
     }
 
-
-    void buildMaxHeap(){
-        for (int i = (int) Math.floor((length)/2); i>= 1;i--){
+    void buildMaxHeap() {
+        for (int i = (int) Math.floor((length)/2); i>= 1;i--) {
             maxHeapify(i);
         }
     }
 
-    private void maxHeapify(int i){
+    private void maxHeapify(int i) {
         int l = leftChild(i);
         int r = rightChild(i);
         int largest;
         try {
             if (l <= heapSize && array[l] > array[i]) {
                 largest = l;
-            } else{
+            } else {
                 largest = i;
             }
             if (r <= heapSize && array[r] > array[largest]) {
                 largest = r;
             }
-            if(largest != i){
+            if (largest != i){
                 exchange(i,largest);
                 maxHeapify(largest);
             }
-        }catch (ArrayIndexOutOfBoundsException e){
+        } catch (ArrayIndexOutOfBoundsException e) {
         }
-
-
     }
 
-    private void exchange(int parent, int largest){
+    private void exchange(int parent, int largest) {
         int temp = array[largest];
         array[largest]= array[parent];
         array[parent] = temp;
     }
 
-    private int leftChild(int parent){
+    private int leftChild(int parent) {
         return 2*parent;
     }
 
-    private int rightChild(int parent){
+    private int rightChild(int parent) {
         return 2*parent+1;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -24,7 +24,6 @@ public class Main implements DS1Interface  {
             key = array[j];
 
             while (i >= 0 && array[i] > key) {
-
                 array[i+1] = array[i];
                 i = i-1;
             }
@@ -36,7 +35,7 @@ public class Main implements DS1Interface  {
     @Override
     public int[] mergeSort(int[] input) {
         array = new int[input.length];
-        for(int i = 0; i<input.length; i++){
+        for (int i = 0; i<input.length; i++){
             array[i] = input[i];
         }
         mergeSortImplementation(0,input.length-1);
@@ -45,7 +44,7 @@ public class Main implements DS1Interface  {
 
 
     private void mergeSortImplementation(int p, int r){
-        if(p<r) {
+        if (p<r) {
             int middleOfArray = (int) Math.floor((p + r) / 2);
             mergeSortImplementation(p, middleOfArray);
             mergeSortImplementation(middleOfArray + 1, r);
@@ -53,7 +52,7 @@ public class Main implements DS1Interface  {
         }
     }
 
-    private void merge(int p, int q, int r){
+    private void merge(int p, int q, int r) {
         int leftArrayMax = q-p+1;
         int rightArrayMax = r-q;
         int[] leftArray = new int[leftArrayMax+1];
@@ -62,20 +61,20 @@ public class Main implements DS1Interface  {
         rightArray[rightArrayMax] = Integer.MAX_VALUE;
         leftArray[leftArrayMax] = Integer.MAX_VALUE;
 
-        for(int i = 0;i< leftArrayMax;i++){
+        for (int i = 0;i< leftArrayMax;i++){
             leftArray[i] = array[p+i];
         }
-        for(int j = 0;j< rightArrayMax;j++){
+        for (int j = 0;j< rightArrayMax;j++){
             rightArray[j] = array[q+1+j];
         }
 
         int rightArrayIndex = 0;
         int leftArrayIndex = 0;
-        for(int k = p;k<=r; k++){
+        for (int k = p;k<=r; k++){
             if (leftArray[leftArrayIndex]<= rightArray[rightArrayIndex]) {
                 array[k] = leftArray[leftArrayIndex];
                 leftArrayIndex+=1;
-            }else {
+            } else {
                 array[k] = rightArray[rightArrayIndex];
                 rightArrayIndex += 1;
             }
